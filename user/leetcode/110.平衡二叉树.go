38ae32921bd4fc8b675abf2249e57a66a56fd92c
CaoGongHui <847781669@qq.com>
2021-10-21 14:34:54 +0800

    添加110 平衡二叉树的go题解

--- user/leetcode/110.平衡二叉树.go
+++ user/leetcode/110.平衡二叉树.go
@@ -7,29 +7,65 @@
 // @lc code=start
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
  */
-class Solution {
-public:
-    int depth(TreeNode* t){
-        if(!t)return 0;
-        return max(depth(t->left), depth(t->right))+1;
-    }
-    bool isBalanced(TreeNode* t) {
-        if(!t)return true;
-        int l = depth(t->left);
-        int r = depth(t->right);
-        if(abs(l-r) > 1){
-            return false;
-        }
-        return isBalanced(t->left) && isBalanced(t->right);
-    }
-};
+// 自顶向下
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	l := depth(root.Left)
+	r := depth(root.Right)
+	if l-r > 1 || l-r < -1 {
+		return false
+	}
+	return isBalanced(root.Left) && isBalanced(root.Right)
+}
+func depth(node *TreeNode) int {
+	if node == nil {
+		return 0
+	}
+	return max(depth(node.Left), depth(node.Right)) + 1
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 // @lc code=end
+//自底向上 优化
+func isBalanced(root *TreeNode) bool {
+	return height(root) >= 0
+}
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftHeight := height(root.Left)
+	if leftHeight == -1 {
+		return -1
+	}
+	rightHeight := height(root.Right)
+	if rightHeight == -1 || abs(leftHeight-rightHeight) > 1 {
+		return -1
+	}
+	return max(leftHeight, rightHeight) + 1
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	}
+	return x
+}
